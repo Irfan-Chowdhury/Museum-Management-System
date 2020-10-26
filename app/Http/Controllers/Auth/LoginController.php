43dd1cc11a86288918591f67445bbf71cb3a5af8
Irfan-Chowdhury <irfanchowdhury80@gmail.com>
2020-10-26 13:24:17 +0600

    Updated Authentication & Images

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -40,34 +42,63 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    // public function login(Request $request)
-    // {   
-    //     $input = $request->all();
-   
-    //     $this->validate($request, [
-    //         'email' => 'required|email',
-    //         'password' => 'required',
-    //     ]);
+    public function login(Request $request)
+    {
+        if (count(User::all()) == 0) {
+            $admin          = new User();
+            $admin->name    = 'Admin';
+            $admin->email   = 'admin@gmail.com';
+            $admin->phone   = '1829498634';
+            $admin->address = 'Chittagong';
+            $admin->role    = 'super-admin'; 
+            $admin->password= Hash::make('admin@gmail.com');
+            $admin->save();
+
+            return redirect()->back();
+        }
+
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        //New Logic Added - "Super-Admin" & "sub-admin" can access but "User" Cann't access .
+        if ($this->attemptLogin($request)) {
+            if ((auth()->user()->role == "super-admin") || (auth()->user()->role == "sub-admin")) 
+            {
+                // return redirect()->route('admin.home');
+                return $this->sendLoginResponse($request);
+            }
+            elseif (auth()->user()->role == "user") {
+                return redirect()->route('login')->send();
+            }            
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
 
 
-   
-    //     if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password'])))
-    //     {
-    //         if ((auth()->user()->role2 == 1) || (auth()->user()->role2 == 2)) {
-    //             return redirect()->route('admin.home');
-    //         }
-    //         elseif (auth()->user()->role2 == 0) {
-    //             return redirect()->route('login')->send();
-    //         }
-    //         else {
-    //             return redirect()->route('login')->send();
-    //         }
-    //         // else{
-    //         //     // return redirect()->route('login')
-    //         //     //     ->with('error','Email-Address And Password Are Wrong.');
-                
-    //         // }
-            
-    //     }          
-    // }
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        // return $this->loggedOut($request) ?: redirect('/');
+        return $this->loggedOut($request) ?: redirect('/login');
+    }
 }
